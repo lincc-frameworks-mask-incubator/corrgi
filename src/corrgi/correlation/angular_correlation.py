af459fa1cee461065de255ad23a2cb0238f26bde
Sandro Campos <scampos@andrew.cmu.edu>
2024-07-08 13:43:06 -0400

    Simplify arg construction

--- src/corrgi/correlation/angular_correlation.py
+++ src/corrgi/correlation/angular_correlation.py
@@ -17,21 +17,14 @@ class AngularCorrelation(Correlation):
         return cff.mod.th_C_wg_naiveway if self.use_weights else cff.mod.th_C_naiveway
 
     def _construct_auto_args(self, df: pd.DataFrame, catalog_info: CatalogInfo) -> list:
+        args = [
+            len(df),
+            *self.get_coords(df, catalog_info),  # cartesian coordinates
+            self.nbins,  # number of angular separation bins
+            self.bins,  # bins in angular separation [deg]
+        ]
         if self.use_weights:
-            args = [
-                len(df),
-                df["wei"].to_numpy(),
-                *self.get_coords(df, catalog_info),  # cartesian coordinates
-                self.nbins,  # number of angular separation bins
-                self.bins,  # bins in angular separation [deg]
-            ]
-        else:
-            args = [
-                len(df),
-                *self.get_coords(df, catalog_info),  # cartesian coordinates
-                self.nbins,  # number of angular separation bins
-                self.bins,  # bins in angular separation [deg]
-            ]
+            args = [args[0], df["wei"].to_numpy(), *args[1:]]
         return args
 
     def _construct_cross_args(
@@ -41,24 +34,14 @@ class AngularCorrelation(Correlation):
         left_catalog_info: CatalogInfo,
         right_catalog_info: CatalogInfo,
     ) -> list:
+        args = [
+            len(left_df),  # number of particles of the left partition
+            *self.get_coords(left_df, left_catalog_info),  # X,Y,Z coordinates of particles
+            len(right_df),  # number of particles of the right partition
+            *self.get_coords(right_df, right_catalog_info),  # X,Y,Z coordinates of particles
+            self.nbins,  # number of angular separation bins
+            self.bins,  # bins in angular separation [deg]
+        ]
         if self.use_weights:
-            args = [
-                len(left_df),  # number of particles of the left partition
-                left_df["wei"].to_numpy(),
-                *self.get_coords(left_df, left_catalog_info),  # X,Y,Z coordinates of particles
-                len(right_df),  # number of particles of the right partition
-                right_df["wei"].to_numpy(),
-                *self.get_coords(right_df, right_catalog_info),  # X,Y,Z coordinates of particles
-                self.nbins,  # number of angular separation bins
-                self.bins,  # bins in angular separation [deg]
-            ]
-        else:
-            args = [
-                len(left_df),  # number of particles of the left partition
-                *self.get_coords(left_df, left_catalog_info),  # X,Y,Z coordinates of particles
-                len(right_df),  # number of particles of the right partition
-                *self.get_coords(right_df, right_catalog_info),  # X,Y,Z coordinates of particles
-                self.nbins,  # number of angular separation bins
-                self.bins,  # bins in angular separation [deg]
-            ]
+            args = [args[0], left_df["wei"].to_numpy(), *args[1:5], right_df["wei"].to_numpy(), *args[5:]]
         return args
